b8ddfbcb44d85e05bcbdedae44dfd3a9ac40e5de
Ares2k <40499701+Ares2k@users.noreply.github.com>
2020-04-22 12:01:08 +0100

    added more accessor methods/toString method

--- java/src/ie/tudublin/Task.java
+++ java/src/ie/tudublin/Task.java
@@ -1,10 +1,12 @@
 package ie.tudublin;
 
+import processing.data.TableRow;
+
 public class Task {
 
     private String task;
-    private String start;
-    private String end;
+    private int start;
+    private int end;
 
     public void setTask(String task) {
         this.task = task;
@@ -12,5 +14,35 @@ public class Task {
     
     public String getTask() {
         return this.task;
+    }
+
+    public void setStart(int start) {
+        this.start = start;
+    }
+
+    public int getStart() {
+        return this.start;
+    }
+
+    public void setEnd(int end) {
+        this.end = end;
+    }
+
+    public int getEnd() {
+        return this.end;
+    }
+
+    public Task(String task, int start, int end) {
+        this.task = task;
+        this.start = start;
+        this.end = end;
+    }
+
+    public Task(TableRow tr) {
+        this(tr.getString("Task"), tr.getInt("Start"), tr.getInt("End"));
+    }
+
+    public String toString() {
+        return "Task name: " + task + ", Start: " + start + ", End: " + end;
     }
 }   
