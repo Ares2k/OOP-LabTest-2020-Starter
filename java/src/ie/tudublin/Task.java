d560b28b3a699040f2b43f2d7439f8f7ae222c67
Ares2k <40499701+Ares2k@users.noreply.github.com>
2020-04-22 11:53:09 +0100

    added accessor methods

---
+++ java/src/ie/tudublin/Task.java
@@ -0,0 +1,16 @@
+package ie.tudublin;
+
+public class Task {
+
+    private String task;
+    private String start;
+    private String end;
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+    
+    public String getTask() {
+        return this.task;
+    }
+}   
