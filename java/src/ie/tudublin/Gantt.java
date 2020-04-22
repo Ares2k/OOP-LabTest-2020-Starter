00c4741f6b0f52078d03f704c5c23a61bbd047d9
Ares2k <40499701+Ares2k@users.noreply.github.com>
2020-04-22 14:28:34 +0100

    Added mouse drag and mouse pressed to cater for dragging the bar charts to increase/decrease size

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -9,8 +9,11 @@ public class Gantt extends PApplet
 {	
 
 	public ArrayList<Task> tasks = new ArrayList<Task>();
+	private int leftSidePressed = -1;
+	private int rightSidePressed = -1;
 	private float leftMarg;
 	private float marg;
+	int days = 30;
 	
 	public void settings()
 	{
@@ -37,12 +40,10 @@ public class Gantt extends PApplet
 
 	public void displayTasks() {
 
-		int days = 30;
 		float rectHeight = 50;
 		float rectStart, rectEnd, rectWidth;
+		float fontSize = 0.8f;
 		float radius = 6;
-		float c;
-		float x;
 
 		stroke(255);
 		fill(255);
@@ -50,24 +51,24 @@ public class Gantt extends PApplet
 
 		for(int i=1; i<=days; i++) {
 
-			c = map(i, 1, 30, leftMarg, width - marg);
+			float c = map(i, 1, 30, leftMarg, width - marg);
 			line(c, marg, c, height - marg);
-			text(i, c, marg * 0.8f);
+			text(i, c, marg * fontSize);
 		}
 
 		for(int i=0;  i<tasks.size(); i++) {
 
 			fill(255);
-			x = map(i, 0, tasks.size(), 2 * marg, height - marg);
+			float x = map(i, 0, tasks.size(), 2 * marg, height - marg);
 			text(tasks.get(i).getTask(), marg, x);
 
 			noStroke();
 			float color = map(i, 0, tasks.size(), 0, 255);
 
 			fill(color, 255, 255);
-			rectStart = map(tasks.get(i).getStart(), 1, 30, leftMarg, width - marg);
-			rectEnd = map(tasks.get(i).getEnd(), 1, 30, leftMarg, width - marg);
-			rectWidth = (int) (rectEnd - rectStart);
+			rectStart = map(tasks.get(i).getStart(), 1, days, leftMarg, width - marg);
+			rectEnd = map(tasks.get(i).getEnd(), 1, days, leftMarg, width - marg);
+			rectWidth = (rectEnd - rectStart);
 
 			float s = (rectHeight / 2);
 			rect(rectStart, x - s, rectWidth, rectHeight, radius);
@@ -77,14 +78,65 @@ public class Gantt extends PApplet
 	public void mousePressed()
 	{
 		println("Mouse pressed");
+		
+		float rectHeight = 50;
+		float x1, x2, y1, y2;
+		float size = 20;
+
+		for(int i=0; i<tasks.size(); i++) {
+			
+			float halfHeight = rectHeight / 2;
+			
+			Task task = tasks.get(i);
+			x1 = map(task.getStart(), 1, 30, leftMarg, width - marg);
+			x2 = map(task.getEnd(), 1, 30, leftMarg, width - marg);
+			
+			y1 = map(i, 0, tasks.size(), 2 * marg, height - marg) - halfHeight;
+			y2 = rectHeight + y1;
+
+			if(mouseY >= y1 && mouseY <= y2) {
+
+				if(mouseX > x1 - size && mouseX < x1 + size) {
+					
+					rightSidePressed = -1;
+					leftSidePressed = i;
+
+				} else if (mouseX < x2 + size && mouseX > x2 - size) {
+
+					rightSidePressed = i;
+					leftSidePressed = -1;
+				}
+			}
+		}
 	}
 
 	public void mouseDragged()
 	{
 		println("Mouse dragged");
+
+		int date;
+
+		if(leftSidePressed > -1) {
+			
+			Task task = tasks.get(leftSidePressed);
+			date = (int) map(mouseX, 0, width, 0, days);
+
+			if(task.getEnd() - date >= 1 && date > 0 && date < task.getEnd()) {
+				task.setStart(date);
+			}
+
+		} else if (rightSidePressed > -1) {
+
+			Task task = tasks.get(rightSidePressed);
+			date = (int) map(mouseX, 0, width, 0, days);
+
+			if(date - task.getStart() >= 1 && date <= days && date > task.getStart()) {
+
+				task.setEnd(date);
+			}
+		}
 	}
 
-	
 	public void setup() 
 	{
 		leftMarg = width / 6;
